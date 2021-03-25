863dfd337b6c3eba6381c2c547759515cb6e77d6
Daniel Ross <mr.danielross@gmail.com>
2021-03-25 08:59:57 +0000

    updates

--- djr-custom.el
+++ djr-custom.el
@@ -3,9 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil)
+ '(aquamacs-customization-version-id 312)
+ '(aquamacs-tool-bar-user-customization nil)
  '(column-number-mode t)
  '(css-indent-offset 2)
  '(custom-enabled-themes '(deeper-blue))
+ '(custom-safe-themes
+   '("da31d302512d3642bc14c68a5040beae151b0b88b79aabde718141c149027563" "81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" default))
  '(debug-on-error t)
  '(dired-sidebar-icon-scale 0.1)
  '(dired-sidebar-mode-line-format
@@ -16,26 +21,28 @@
  '(display-line-numbers t)
  '(fringe-mode '(nil . 0) nil (fringe))
  '(global-hl-line-mode t)
+ '(global-linum-mode t)
  '(indicate-buffer-boundaries 'left)
  '(inhibit-startup-buffer-menu t)
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(js-indent-level 2)
  '(markdown-command "/usr/local/bin/pandoc")
+ '(ns-tool-bar-display-mode 'both)
+ '(ns-tool-bar-size-mode 'regular)
+ '(one-buffer-one-frame-mode nil)
  '(org-agenda-files (list org-directory))
  '(org-directory (expand-file-name "~/org"))
  '(org-log-into-drawer nil)
  '(package-selected-packages
-<<<<<<< HEAD
-   '(scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile vscode-icon dired-sidebar add-node-modules-path company eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd zoom prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui lsp-mode json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move whitespace-cleanup-mode magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime tabbar auctex ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
-=======
-   '(reformatter xah-replace-pairs xah-css-mode eslint-fix cl-lib emacs-sos projectile vscode-icon dired-sidebar add-node-modules-path company eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd zoom prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui lsp-mode json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move whitespace-cleanup-mode magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime tabbar auctex ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
->>>>>>> 8c44a79f62643b1d34a0202da41219e65fe12156
+   '(reformatter highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile vscode-icon dired-sidebar add-node-modules-path company eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd zoom prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui lsp-mode json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move whitespace-cleanup-mode magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime tabbar auctex ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
  '(safe-local-variable-values '((base . 10) (package . clm) (syntax . common-lisp)))
  '(show-paren-mode t)
+ '(tabbar-mode t)
  '(telephone-line-mode t)
  '(tool-bar-mode nil)
  '(typescript-indent-level 2)
+ '(visual-line-mode nil t)
  '(zoom-mode t nil (zoom)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -45,3 +52,4 @@
  '(hl-line ((t (:background "gray20"))))
  '(telephone-line-accent-active ((t (:inherit mode-line :background "coral3" :foreground "white")))))
 (put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
