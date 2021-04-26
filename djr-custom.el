c097f29eb434cc7b24ecbd547e9e9be5062e04ad
Daniel Ross <mr.danielross@gmail.com>
2021-04-26 15:01:39 +0100

    Before save hook remove leading decimal zero for css mode

--- djr-custom.el
+++ djr-custom.el
@@ -29,7 +29,7 @@
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(js-indent-level 2)
- '(markdown-command "/usr/local/bin/pandoc")
+ '(markdown-command "/usr/local/bin/pandoc" t)
  '(ns-tool-bar-display-mode 'both)
  '(ns-tool-bar-size-mode 'regular)
  '(one-buffer-one-frame-mode nil)
@@ -38,7 +38,7 @@
  '(org-log-into-drawer nil)
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(multiple-cursors org-jira command-log-mode auto-package-update reformatter highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile vscode-icon dired-sidebar add-node-modules-path company eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd zoom prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui lsp-mode json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move whitespace-cleanup-mode magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime tabbar auctex ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
+   '(shader-mode multiple-cursors org-jira command-log-mode auto-package-update reformatter highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile vscode-icon dired-sidebar add-node-modules-path company eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd zoom prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui lsp-mode json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move whitespace-cleanup-mode magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime tabbar auctex ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
  '(safe-local-variable-values '((base . 10) (package . clm) (syntax . common-lisp)))
  '(show-paren-mode t)
  '(tabbar-mode t)
