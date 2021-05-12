eeec529c2ca335651390d638ca6fc56a00403cbc
Daniel Ross <mr.danielross@gmail.com>
2021-05-12 19:41:25 +0100

    Remove zoom customs

--- djr-custom.el
+++ djr-custom.el
@@ -35,15 +35,14 @@
  '(org-log-into-drawer nil)
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(gnu-elpa-keyring-update multiple-cursos shader-mode multiple-cursors org-jira command-log-mode auto-package-update highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile dired-sidebar add-node-modules-path eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
+   '(reformatter lsp-mode company gnu-elpa-keyring-update multiple-cursos shader-mode multiple-cursors org-jira command-log-mode auto-package-update highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile dired-sidebar add-node-modules-path eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
  '(safe-local-variable-values '((base . 10) (package . clm) (syntax . common-lisp)))
  '(show-paren-mode t)
  '(telephone-line-mode t)
  '(tool-bar-mode nil)
  '(typescript-indent-level 2)
  '(visual-line-mode nil t)
- '(web-mode-enable-auto-quoting nil)
- '(zoom-mode t nil (zoom)))
+ '(zoom-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
