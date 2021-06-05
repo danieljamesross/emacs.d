84f2688478d2a06ea4cda1b3aef0ffd2bdbffd01
Daniel Ross <mr.danielross@gmail.com>
2021-06-05 10:15:45 +0100

    Native comp and better TS

--- djr-custom.el
+++ djr-custom.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(css-indent-offset 2 t)
  '(custom-enabled-themes '(deeper-blue))
  '(custom-safe-themes
    '("da31d302512d3642bc14c68a5040beae151b0b88b79aabde718141c149027563" "81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" default))
@@ -35,12 +34,12 @@
  '(org-log-into-drawer nil)
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(reformatter lsp-mode company gnu-elpa-keyring-update multiple-cursos shader-mode multiple-cursors org-jira command-log-mode auto-package-update highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile dired-sidebar add-node-modules-path eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
+   '(lsp-treemacs preview auctex latex tex-site reformatter lsp-mode company gnu-elpa-keyring-update multiple-cursos shader-mode multiple-cursors org-jira command-log-mode auto-package-update highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile dired-sidebar add-node-modules-path eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
+ '(prettier-js-show-errors 'buffer)
  '(safe-local-variable-values '((base . 10) (package . clm) (syntax . common-lisp)))
  '(show-paren-mode t)
  '(telephone-line-mode t)
  '(tool-bar-mode nil)
- '(typescript-indent-level 2)
  '(visual-line-mode nil t)
  '(zoom-mode t))
 (custom-set-faces
@@ -53,3 +52,4 @@
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
