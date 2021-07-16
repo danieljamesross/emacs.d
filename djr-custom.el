2c9e92a9114e76fe5bbc6ef88cfb7ba47e3bcd5d
Daniel Ross <mr.danielross@gmail.com>
2021-07-16 09:22:04 +0100

    Updated custom file

--- djr-custom.el
+++ djr-custom.el
@@ -25,7 +25,7 @@
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(js-indent-level 2)
- '(markdown-command "/usr/local/bin/pandoc")
+ '(markdown-command "/usr/local/bin/pandoc" t)
  '(ns-tool-bar-display-mode 'both)
  '(ns-tool-bar-size-mode 'regular)
  '(one-buffer-one-frame-mode nil)
@@ -34,8 +34,7 @@
  '(org-log-into-drawer nil)
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(lsp-treemacs preview auctex latex tex-site reformatter lsp-mode company gnu-elpa-keyring-update multiple-cursos shader-mode multiple-cursors org-jira command-log-mode auto-package-update highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile dired-sidebar add-node-modules-path eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
- '(prettier-js-show-errors 'buffer)
+   '(bash-completion company lsp-treemacs preview auctex latex tex-site reformatter gnu-elpa-keyring-update multiple-cursos shader-mode multiple-cursors org-jira command-log-mode auto-package-update highlight-indent-guides eslint-fix fill-column-indicator quelpa-use-package quelpa use-package dracula-theme kintaro kintaro-mode scss-mode lorem-ipsum yaml-mode jinja2-mode cl-lib emacs-sos projectile dired-sidebar add-node-modules-path eshell-syntax-highlighting tide fira-code-mode unicode-fonts auto-indent-mode org-bullets xkcd prettier-js sos paredit js-auto-beautify csv-mode default-text-scale lsp-ui json-mode js2-mode transpose-frame flycheck emmet-mode js-comint fast-scroll buffer-move magit multi-term telephone-line exec-path-from-shell latex-pretty-symbols latex-preview-pane markdown-mode ac-slime ox-reveal dimmer slime-repl-ansi-color rainbow-delimiters smartparens lisp-extra-font-lock web-mode auto-complete slime))
  '(safe-local-variable-values '((base . 10) (package . clm) (syntax . common-lisp)))
  '(show-paren-mode t)
  '(telephone-line-mode t)
