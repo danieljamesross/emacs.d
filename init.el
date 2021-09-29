5c817cf45a52fb716909687fa3728015a68ad3ea
Daniel Ross <mr.danielross@gmail.com>
2021-09-29 16:39:11 +0100

    Format init.el

--- init.el
+++ init.el
@@ -5,25 +5,31 @@
   (message "Emacs loaded in %s with %d garbage collections."
            (format "%.2f seconds"
                    (float-time
-                     (time-subtract after-init-time before-init-time)))
+                    (time-subtract after-init-time before-init-time)))
            gcs-done))
 
 (add-hook 'emacs-startup-hook #'efs/display-startup-time)
 
 (require 'package)
-(setq package-enable-at-startup 't)
+
+(setq package-enable-at-startup t)
+
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
+  (message "no use-package, installing...")
   (package-refresh-contents)
   (package-install 'use-package))
+
 (defadvice en/disable-command (around put-in-custom-file activate)
   "Put declarations in `custom-file'."
   (let ((user-init-file custom-file))
     ad-do-it))
-(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "README.org")))
 
 (server-start)
+
 ;; Make gc pauses faster by decreasing the threshold.
 (setq gc-cons-threshold (* 2 1000 1000))
