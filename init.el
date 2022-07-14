759c1474d7880dfa1ed543d4387769c1e808a674
Dan Ross <mr.danielross@gmail.com>
2022-07-14 09:03:38 +0100

    Update gc-cons-threshold

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold (* 100 1024 1024))
+
+(setq user-emacs-directory (expand-file-name "~/.emacs.d/"))
 
 (defun efs/display-startup-time ()
   (message "Emacs loaded in %s with %d garbage collections."
@@ -70,4 +72,4 @@
   (server-start))
 
 ;; Make gc pauses faster by decreasing the threshold.
-(setq gc-cons-threshold (* 2 1000 1000))
+(setq gc-cons-threshold (* 2 1024 1024))
