ded35f68adda51fe17073a1c0f393fea3366910b
Daniel J. Ross <mr.danielross@gmail.com>
2024-09-17 00:59:59 +0100

    Update custom files

--- djr-custom.el
+++ djr-custom.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,8 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (list org-directory))
  '(org-directory (expand-file-name "~/org"))
- '(package-selected-packages
-   '(clang-format json-mode vterm lsp-mode selectrum restclient dired-details webkit doom-modeline string-inflection coverlay tide rjsx-mode prettier-js js-comint add-node-modules-path buffer-move exec-path-from-shell dired-rainbow auto-package-update quelpa-use-package quelpa transpose-frame tree-sitter-langs whitespace-cleanup-mode highlight-indent-guides fast-scroll yaml-mode web-mode vertico use-package typescript-mode smart-hungry-delete slime shx shader-mode selectrum-prescient sass-mode reveal-in-osx-finder rainbow-delimiters projectile polymode ox-reveal org-bullets orderless multiple-cursors multi-vterm marginalia magit lsp-ui lsp-treemacs lorem-ipsum lisp-extra-font-lock latex-preview-pane js2-mode iedit graphql-mode flycheck expand-region eshell-syntax-highlighting embark-consult editorconfig doom-themes dockerfile-mode dimmer diff-hl corfu bash-completion auctex all-the-icons-ibuffer ace-jump-mode))
+ '(package-selected-packages nil)
  '(zoom-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
