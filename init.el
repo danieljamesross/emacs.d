5b79d5aa7aecd5ba80d4a0c8bdd46b70f04fffb9
Dan <dan@iris.audio>
2024-09-22 16:51:11 +0100

    Add straight.el to init.el

--- init.el
+++ init.el
@@ -63,6 +63,22 @@
 
 (setq use-package-always-ensure (internet-up-p))
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (defadvice en/disable-command (around put-in-custom-file activate)
   "Put declarations in `custom-file'."
   (let ((user-init-file custom-file))
