4c2b83b2b012dc79d8a5ae394bacf478e13e2093
Dan <dan@iris.audio>
2024-09-22 16:52:08 +0100

    Update custom

--- djr-custom.el
+++ djr-custom.el
@@ -4,9 +4,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(global-display-line-numbers-mode t)
  '(org-agenda-files (list org-directory))
+ '(org-babel-python-command "python3")
  '(org-directory (expand-file-name "~/org"))
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(cmake-font-lock cmake-mode cmake magit duplicate-thing duplicate-line json-mode ggtags slime-repl-ansi-color ibuffer-project lms xkcd doom-modeline clang-format vterm lsp-mode selectrum restclient dired-details webkit string-inflection coverlay tide rjsx-mode prettier-js js-comint add-node-modules-path buffer-move exec-path-from-shell dired-rainbow auto-package-update quelpa-use-package quelpa transpose-frame tree-sitter-langs whitespace-cleanup-mode highlight-indent-guides fast-scroll yaml-mode web-mode vertico use-package typescript-mode smart-hungry-delete slime shx shader-mode selectrum-prescient sass-mode reveal-in-osx-finder rainbow-delimiters projectile polymode ox-reveal org-bullets orderless multiple-cursors multi-vterm marginalia lsp-ui lsp-treemacs lorem-ipsum lisp-extra-font-lock latex-preview-pane js2-mode iedit flycheck expand-region eshell-syntax-highlighting editorconfig doom-themes dockerfile-mode dimmer diff-hl corfu bash-completion all-the-icons-ibuffer ace-jump-mode))
+ '(safe-local-variable-values '((Base . 10) (Package . CL-PPCRE) (Syntax . COMMON-LISP)))
+ '(tool-bar-mode nil)
  '(zoom-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
